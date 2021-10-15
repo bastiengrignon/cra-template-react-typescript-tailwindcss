ed24278f3f2f2eb1bbae40c6db843200f7615d7a
bastiengrignon <basgrignon@gmail.com>
2021-10-15 10:14:25 +0200

    Configure ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => (
+const App: React.FC = () => (
     <div className="m-10">
         <p className="text-center text-blue-500 text-4xl">New App</p>
     </div>
